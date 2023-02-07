ad6ad50355783c0f4915e05e1b0828985a5fbeac
Kelvin Purdom <kelvin.purdom@gmail.com>
2023-02-07 11:37:49 +0100

    restructured prediction.py

--- prediction.py
+++ prediction.py
@@ -1,6 +1,38 @@
-import keras
 
-def predict(data):
-    #rf = joblib.load('pipe_rf_model.sav')
-    model = keras.models.load_model('my_model.h5')
-    return model.predict(data)
+import pickle
+import numpy as np
+from tensorflow import keras
+from keras.preprocessing.text import Tokenizer
+from keras.utils import pad_sequences
+
+
+max_sequence_len = 88
+
+model = keras.models.load_model('my_model.h5')
+with open('notebooks/tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+
+
+def make_lyrics(seed_text, next_words):
+    pred_index=[]
+    for i in range(next_words):
+        token_list = tokenizer.texts_to_sequences([seed_text])[0]
+        token_list = pad_sequences([token_list],
+                     maxlen=max_sequence_len-1,padding='pre')
+        #print(token_list.shape)
+        token_list = np.reshape(token_list, (1, max_sequence_len-1, 1))
+        predicted = model.predict(token_list, verbose=0)
+        predicted_index =  np.argmax(predicted)
+        pred_index.append(predicted_index)
+
+
+
+        #predicted_index=1
+        output_word = ""
+        for word, index in tokenizer.word_index.items():
+            if index == predicted_index:
+                output_word = word
+                break
+        seed_text += " " + output_word
+    print(seed_text)
+    return seed_text
