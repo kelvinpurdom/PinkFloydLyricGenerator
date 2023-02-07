37f1821213342adc41823039a24768a8ec5887fa
Kelvin Purdom <kelvin.purdom@gmail.com>
2023-02-07 12:23:01 +0100

    updated streamlit

--- streamlit.py
+++ streamlit.py
@@ -6,7 +6,6 @@ from model import make_lyrics
 
 
 seed = st.text_input('seed text:', '')
-output_length = st.number_input('Sentence length:', min_value=1, max_value=88, value=10, step=1)
 
 
-st.text(make_lyrics(seed, output_length))
+st.text(make_lyrics(seed, 7))
