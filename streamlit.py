1616ca38b9c908d451f4e995d94d8dac986b17e4
Kelvin Purdom <kelvin.purdom@gmail.com>
2023-02-07 12:21:13 +0100

    streamlit sucessfully working globally

--- streamlit.py
+++ streamlit.py
@@ -5,7 +5,8 @@ from model import make_lyrics
 
 
 
-title = st.text_input('seed text:', '')
+seed = st.text_input('seed text:', '')
+output_length = st.number_input('Sentence length:', min_value=1, max_value=88, value=10, step=1)
 
 
-st.text(make_lyrics(title, 7))
+st.text(make_lyrics(seed, output_length))
